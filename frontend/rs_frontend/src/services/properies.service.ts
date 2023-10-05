88f7b9009351471f3464055de6ba15383a829d9e
Felix Alm <carritohmc@gmail.com>
2023-10-04 22:17:39 -0400

    fixed the slider, has images integrated with Rafa's changes

--- frontend/rs_frontend/src/services/properies.service.ts
+++ frontend/rs_frontend/src/services/properies.service.ts
@@ -4,14 +4,23 @@ import http from './http-common';
 class PropertiesService {
 
   async getAllProperties() {
-    
+
     try {
-        const response = await http.get('/properties/'); // Uncomment for production
-        // const response = testData;
-        console.log(response);
-        return response;
+      const response = await http.get('/properties/'); // Uncomment for production
+      // const response = testData;
+      return response;
     } catch (error) {
-        console.error(error);
+      console.error(error);
+    }
+  }
+
+  async getProperty(id: string) {
+    try {
+      const response = await http.get(`/properties/${id}/`); // Uncomment for production
+      // const response = testData;
+      return response;
+    } catch (error) {
+      console.error(error);
     }
   }
 }
